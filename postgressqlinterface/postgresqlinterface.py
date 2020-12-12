dd37acd0997789bc6d7e7efa672882aa96fb7b68
Antonio Benjumea <73562533+antjes88@users.noreply.github.com>
2020-12-12 18:00:56 +0000

    Creation of method delete_from_table & improving methods doc. (#3)
    * creating test branch
    
    * creating file to push to pypi live
    
    * creation of delete_from_table method and improving methods docs.

--- postgressqlinterface/postgresqlinterface.py
+++ postgressqlinterface/postgresqlinterface.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 class PostgreSQL:
-    """Connect and interact with a posgreSQL database"""
+    """Connect and interact with a PostgreSQL database"""
     def __init__(self, database_url, sslmode='require', host='HEROKU'):
         """
         Initialise the class
@@ -14,12 +14,12 @@ class PostgreSQL:
             self.database_url = database_url
             self.sslmode = sslmode
         else:
-            raise Exception('No valid host has been provided when instantiating Pos')
+            raise Exception('No valid host has been provided when instantiating the class.')
 
     @staticmethod
     def close_connection(cursor, conn):
         """
-        Method to close a database connection to a PosgreSQL database
+        Method to close a database connection to a PostgreSQL database
         """
         if cursor:
             cursor.close()
@@ -28,7 +28,7 @@ class PostgreSQL:
 
     def create_connection(self):
         """
-        Method to create a database connection to a Posgresql database
+        Method to create a database connection to a PostgreSQL database
         :return: Connection object and cursor or None
         """
         cursor, conn = None, None
@@ -47,7 +47,6 @@ class PostgreSQL:
         """
         Method to retrieve data from a table.
         :param statement: sql statement
-
         :return: df: dataframe of the sql
         """
         conn, cursor = None, None
@@ -65,7 +64,7 @@ class PostgreSQL:
 
     def execute(self, statement):
         """
-        Execute a statement in database
+        Execute a sql statement in database
         param execute: parameter to execute
         """
         conn, cursor = None, None
@@ -81,43 +80,79 @@ class PostgreSQL:
     def insert_table(self, table_name, df, print_sql=False):
         """
         This method is to insert new values in a table. It is able to manage insertion of null values.
+
+            INSERT INTO table_name (df.column[0], df.column[1], ... df.column[n])
+                VALUES (df.loc[0, column[0]], df.loc[0, column[1]], ... df.loc[0, column[n]]),
+                       (df.loc[1, column[0]], df.loc[1, column[1]], ... df.loc[1, column[n]]),
+                       .
+                       .
+                       .
+                       (df.loc[n, column[0]], df.loc[n, column[1]], ... df.loc[n, column[n]]);
+
         :param table_name: name of the table where data is going to be inserted, it must include the schema
         :param df: dataframe of values to insert into the table
         :param print_sql: boolean to indicate if sql statement must be print on python console
         :return:
         """
         # Creation of Insert into and list of columns
-        base_statement = 'INSERT INTO %s (' % table_name
+        statement = 'INSERT INTO %s (' % table_name
         for col in df.columns.values.tolist():
-            base_statement += ' %s,' % col
-        base_statement = base_statement[:-1] + ') VALUES ('
+            statement += ' %s,' % col
+        statement = statement[:-1] + ') VALUES '
 
         # this is for nan detection to input NULL values
         nan_df = df.isna()
 
         # creation of insert statements
-        sql_insert = ''
         for iindex in df.index.values.tolist():
-            statement = base_statement
+            statement += ' ('
             for value in df.columns.values.tolist():
                 if nan_df.loc[iindex, value]:
                     statement += " NULL,"
                 else:
                     statement += " '%s'," % df.loc[iindex, value]
-            statement = statement[:-1] + ')'
+            statement = statement[:-1] + '),'
 
-            sql_insert = sql_insert + '; ' + statement
+        statement = statement[:-1] + ';'
 
         if print_sql:
-            print(sql_insert)
+            print(statement)
 
-        self.execute(sql_insert)
+        self.execute(statement)
 
     def update_table(self, table_name, df, where_identifier, print_sql=False):
         """
         This method is to update values in a table taking into account the where_identifier. It creates one UPDATE
         statement for each row in df.
-        :param table_name: name of the table to update
+
+            WITH to_update = [col for col in df.columns if col not in where_identifier]
+                UPDATE table_name
+                SET to_update[0] = df.loc[0, to_update[0]]
+                    .
+                    .
+                    .
+                    to_update[n] = df.loc[0, to_update[n]]
+                WHERE where_identifier[0] = df.loc[0, where_identifier[0]]
+                      .
+                      .
+                      .
+                      where_identifier[n] = df.loc[0, where_identifier[n]];
+                .
+                .
+                .
+                UPDATE table_name
+                SET to_update[0] = df.loc[n, to_update[0]]
+                    .
+                    .
+                    .
+                    to_update[n] = df.loc[n, to_update[n]]
+                WHERE where_identifier[0] = df.loc[n, where_identifier[0]]
+                      .
+                      .
+                      .
+                      where_identifier[n] = df.loc[n, where_identifier[n]];
+
+        :param table_name: name of the table to update included schema
         :param df: dataframe with the data to update in the table
         :param where_identifier: list of columns to list on the where clause
         :param print_sql: boolean to indicate if sql statement must be print on python console
@@ -150,6 +185,63 @@ class PostgreSQL:
                 else:
                     statement += " '%s' " % (df.loc[i, where_identifier[j]])
             statement += '; '
+
+        if print_sql:
+            print(statement)
+
+        self.execute(statement)
+
+    def delete_from_table(self, table_name, df, print_sql=False):
+        """
+        Method to delete rows from a table. It deletes rows from table_name based on the where clause created with
+        values and columns of df.
+
+            IF len(df.columns) == 1:
+                DELETE FROM table_name  WHERE df.column IN (df.values)
+            IF len(df.columns) > 1:
+                DELETE FROM table_name  WHERE df.column[0] == 'df.loc[0, column[0]]'......
+                                            AND df.column[n] == 'df.loc[0, column[n]]';
+                .
+                .
+                .
+                DELETE FROM table_name  WHERE df.column[0] == 'df.loc[n, column[0]]'......
+                                            AND df.column[n] == 'df.loc[n, column[n]]';
+
+        clause
+        :param table_name: name of the table to update included schema
+        :param df: dataframe with the columns of the table to be included on the where clause
+        :param print_sql: boolean to indicate if sql statement must be print on python console
+        :return:
+        """
+        if df.shape[0] == 0:
+            raise Exception("Please, provide a non-empty dataframe.")
+
+        elif len(df.columns.to_list()) == 1:
+            col = df.columns.to_list()[0]
+            nan_df = df.isna()
+            statement = 'DELETE FROM %s WHERE %s IN (' % (table_name, col)
+            for i in df.index.values.tolist():
+                if nan_df.loc[i, col]:
+                    statement += " NULL,"
+                else:
+                    statement += " '%s'," % (df.loc[i, col])
+            statement = statement[:-1] + ')'
+
+        else:
+            nan_df = df.isna()
+            cols = df.columns.to_list()
+            statement = ''
+            for i in df.index.values.tolist():
+                statement += ' DELETE FROM %s WHERE ' % table_name
+                for j in range(0, len(cols)):
+                    if j > 0:
+                        statement += ' AND '
+                    statement += " %s = " % cols[j]
+                    if nan_df.loc[i, cols[j]]:
+                        statement += " NULL "
+                    else:
+                        statement += " '%s' " % (df.loc[i, cols[j]])
+                statement += '; '
 
         if print_sql:
             print(statement)
--- tests/test_postgressqlinterface.py
+++ tests/test_postgressqlinterface.py
@@ -5,12 +5,8 @@ import pytest
 from dotenv import load_dotenv
 from postgressqlinterface.postgresqlinterface import PostgreSQL
 
-
-"""
-To be able to execute this test statements you need to provide a .env file with the url to connect to a database
-"""
-
-load_dotenv(dotenv_path='.env')
+if os.path.isfile('.env'):
+    load_dotenv(dotenv_path='.env')
 env_var = os.environ['DATABASE_URL']
 
 
@@ -91,3 +87,53 @@ def update():
 
 def test_execute_insert_update_query(update):
     assert update.all().all()
+
+
+###############################################################################################
+
+@pytest.fixture(scope='module')
+def heroku_conn():
+    heroku_db = PostgreSQL(env_var)
+    statement_create_table = """
+                            Drop table if exists test.simple; 
+                            drop schema if exists test;
+                            
+                            CREATE SCHEMA test
+                        
+                            create table test.simple (
+                                Id INT not null,
+                                Name VARCHAR(100) NOT NULL,
+                                Activated BOOLEAN not null,
+                                Date DATE NOT NULL
+                            )
+    """
+    heroku_db.execute(statement_create_table)
+
+    to_insert = pd.DataFrame.from_dict({'id': [1, 2, 3, 4],
+                                        'name': ['Mercedes', 'Toyota', 'Suzuki', 'BMW'],
+                                        'activated': [True, False, False, True],
+                                        'date': [dt.date(2020, 1, 1), dt.date(2020, 2, 2),
+                                                 dt.date(2020, 3, 3), dt.date(2020, 4, 4)]})
+    heroku_db.insert_table('test.simple', to_insert.copy())
+
+    yield heroku_db
+
+    heroku_db.execute("Drop table test.simple; drop schema test")
+
+
+def test_delete_several_cols(heroku_conn):
+    to_delete = pd.DataFrame.from_dict({'id': [1, 2, 3, 4],
+                                        'date': [dt.date(2020, 1, 1), dt.date(2020, 2, 2),
+                                                 dt.date(2020, 3, 3), dt.date(2020, 4, 4)]})
+    heroku_conn.delete_from_table('test.simple', to_delete)
+    simple = heroku_conn.query("SELECT * FROM test.simple")
+
+    assert simple.shape[0] == 0
+
+
+def test_delete_one_col(heroku_conn):
+    to_delete = pd.DataFrame.from_dict({'id': [1, 2, 3, 4]})
+    heroku_conn.delete_from_table('test.simple', to_delete)
+    simple = heroku_conn.query("SELECT * FROM test.simple")
+
+    assert simple.shape[0] == 0
