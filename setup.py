8bdeb796b34958e8c0e3b8bdb41833679bf19e58
$Cuentas-AppServices <antjesben@gmail.com>
2020-12-16 19:48:34 +0000

    fixing error when including package

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='postgresql-interface',
-    packages=find_packages(include=['postgressqlinterface']),
-    version='0.0.4',
+    packages=find_packages(include=['postgressql_interface']),
+    version='0.0.6',
     description='Library to load and extract data from a PosgreSQL Database '
                 'with Python with a simple SQL style language',
     long_description=long_description,
