cae4af55e32dcdd10cc7ae73a7959bc434fd6851
Antonio Benjumea <73562533+antjes88@users.noreply.github.com>
2022-11-06 10:37:03 +0000

    GCP in. Heroku testing out. (#8)
    * Disabled Heroku testing due to charging process for DB services. Enabling GCP entry point and testing.
    
    * adding SA with Cloud SQL Proxy to connect to Cloud SQL
    
    * adding GOOGLE_APPLICATION_CREDENTIALS as env var
    
    * Removing testing before pushing to pypi due to problems to connect through SQL Proxy. Still testing have been done at local.

--- playground.py
+++ playground.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from postgresql_interface.postgresql_interface import postgres_sql_connector_factory
 
+# Heroku
 if not os.path.isfile('.env'):
     raise Exception('You need a file call .env with DATABASE_URL of the Heroku PostgreSQL Database you are going '
                     'to connect to.')
@@ -14,3 +15,22 @@ db_conn = postgres_sql_connector_factory(vendor='heroku', database_url=database_
 
 print(db_conn)
 db_conn.query("SELECT current_date")
+
+
+# GCP
+if not os.path.isfile('.env'):
+    raise Exception('You need a file call .env with DATABASE_URL of the Heroku PostgreSQL Database you are going '
+                    'to connect to.')
+else:
+    load_dotenv(dotenv_path='.env')
+    host = os.environ['HOST']
+    database_name = os.environ['DATABASE_NAME']
+    user_name = os.environ['USER_NAME']
+    user_password = os.environ['USER_PASSWORD']
+    port = os.environ['PORT']
+
+db_conn = postgres_sql_connector_factory(
+    vendor='GCP', host=host, database_name=database_name, user_name=user_name, user_password=user_password, port=port)
+
+print(db_conn)
+a = db_conn.query("SELECT current_date")
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,14 @@ def create_env_variables_heroku():
     if os.path.isfile('.env'):
         load_dotenv(dotenv_path='.env')
     return os.environ['DATABASE_URL']
+
+
+@pytest.fixture(scope='session')
+def create_env_variables_gcp():
+    if os.path.isfile('.env'):
+        load_dotenv(dotenv_path='.env')
+    return {'host': os.environ['HOST'],
+            'database_name': os.environ['DATABASE_NAME'],
+            'user_name': os.environ['USER_NAME'],
+            'user_password': os.environ['USER_PASSWORD'],
+            'port': os.environ['PORT']}
